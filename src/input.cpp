d623c016dfee3769d0b6e9a3940a095ee686f710
J.T. Nunley <xtremegamesjt@gmail.com>
2017-12-15 17:40:36 -0800

    Fixed GNU licensing header in input.cpp

--- src/input.cpp
+++ src/input.cpp
@@ -1,20 +1,21 @@
 /*
- * =====================================================================================
- *
- *       Filename:  input.cpp
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  12/15/2017 05:15:02 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  J.T. Nunley (JaydomStudios), jtnunley01@gmail.com
- *   Organization:  JaydomStudios
- *
- * =====================================================================================
- */
+ 
+This file is part of AVCB.
+
+AVCB is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AVCB is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AVCB.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include "input.h"
 
